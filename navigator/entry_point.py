c2ae2adcd5936055449060d9aed126aa6042a95a
Onur Demircioğlu <59303053+onurdemircioglu@users.noreply.github.com>
2025-06-27 20:37:31 +0300

    Update entry_point.py

--- navigator/entry_point.py
+++ navigator/entry_point.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.markdown("Apps")
+
+col1, col2 = st.columns(2)
+
+with col1:
+
+    st.markdown("""
+    My Works
+    - [Movies TV Shows Tracking](https://app-works-jzt8gzeunzdfjsszheejit.streamlit.app/)
+    
+    - [Personal Finance Assistant](https://www.themoviedb.org)    
+    
+    - [Wheel of Fortune](https://www.rottentomatoes.com)
+    """)
+
+with col2:
+    st.markdown("""
+        Links
+        - [My Blog (Substack)](https://computerdiaries.substack.com/)
+        
+        - [GitHub](https://github.com/onurdemircioglu)
+        """)
